d77890ed75b0af40aa9121a17cbd890b7704d685
JFays <joel.fays@jfays.net>
2015-05-15 19:37:24 +0200

    Action helper

---
+++ app/scripts/controllers/index_controller.js
@@ -0,0 +1,8 @@
+/// <reference path="../../../typings/ember/ember.d.ts"/>
+RocknrollcallYeoman.IndexController = Ember.Controller.extend({
+	actions: {
+		viewedArtist: function(artist) {
+			console.log('hang on I"m viewing: ' + artist.name)
+		}
+	}
+});
