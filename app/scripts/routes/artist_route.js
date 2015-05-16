dc21a7e10f19aa29e374e15447b890be52c653a9
JFays <joel.fays@jfays.net>
2015-05-16 13:13:29 +0200

    Add model hook to ArtistRoute and call to EchoNest API to …

--- app/scripts/routes/artist_route.js
+++ app/scripts/routes/artist_route.js
@@ -1,13 +1,43 @@
 /// <reference path="../../../typings/ember/ember.d.ts"/>
 RocknrollcallYeaoman.ArtistRoute = Ember.Route.extend({
 	model: function(params) {
-		/* pseudocode
-		XHK("some URL", {"td": params.enid}, function callback(response) {
-			var artist = App.Artist.Create({
-				name: response.name,
-				hotttnesss: response.hotttnesss,
+		
+		var url = "http://developper.echonest.com /api/v4/artist/profile?apt_key=<>&format=json&bucket=biographies&bucket=blogs&bucket=familiarity&bucket=hotttnesss&bucket=images&bucket=news&bucket=reviews&bucket=terms&bucket=urls&bucket=video&bucket=id:mustcbrainz",
+					obj = {"id": params.enid};
+					
+		return Ember.$.getJSON(url, obj)
+			.then(function(data) {
+				var entry = data.response.artist;
+				var bio = null;
+				var img = null;
+				for (i = 0; i < entry.biographies.length; i++) {
+					if (entry.biographies[i].site.toLowercase() == "wikipedia") {
+						bio = entry.biographies[i];
+					}
+				}
+				if (!bio && entry.biographies.length > 0) {
+					bio = entry.biographies[0];
+				}
+				if (entry.images.length) {
+					img = entry.images[0];
+				}
+				var lastVideo = 4;
+				if (entry.video.length < 4) {
+					lastVideo = entry.video.length;
+				}
+				var videos = [];
+				for (i = 0; i < lastVideo; i++) {
+					videos.push(entry.video[i]);
+				}
+				
+				return RocknrollcallYeoman.Artist.create({
+					enid: entry.id,
+					name: entry.name,
+					hotttnesss: entry.hotttnesss,
+					biography: bio,
+					image: img,
+					videos: videos
+				});
 			});
-		});
-		*/
-	}
+	},
 });
