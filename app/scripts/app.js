95062b4958a812e6a678ff90040c15fa28e131e1
JFays <joel.fays@jfays.net>
2015-05-16 16:37:48 +0200

    Bug fix

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,37 +8,10 @@ RocknrollcallYeoman.ApplicationController = Em.ObjectController.extend({
 	applicationName: "Rock'n'Roll Call",
 	actions: {
 		submit: function() {
-			this.trasitionToRoute('search-results', this.get('searchTerms'));
+			this.transitionToRoute('search-results', this.get('searchTerms'));
 		}
 	}
 });
-
-
-/*
-RocknrollcallYeoman.dummySearchResultsArtists = [
-	{
-		id: 1,
-		name: 'Tom Waits',
-		type: 'artist',
-		enid: 'ARERLPG1187FB3BB39',
-		hotttnesss: '1'
-	},
-	{
-		id: 2,
-		name: 'Thomas Alan Waits',
-		type: 'artist',
-		enid: 'ARERLPG1187FB3BB39',
-		hotttnesss: '.89'
-	},
-	{
-		id: 3,
-		name: 'Tom Waits w/ Keith Richards',
-		type: 'artist',
-		enid: 'ARMPVNN13CA39CF8FC',
-		hotttnesss: '.79'
-	}
-];
-*/
 
 /* Order and include as you please. */
 require('scripts/controllers/*');
