61d3726ba818f8114e8e6a75709b172f412644cc
JFays <joel.fays@jfays.net>
2015-05-17 11:32:54 +0200

    Add ActivityRoute to the Router.

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,5 +1,8 @@
 RocknrollcallYeoman.Router.map(function () {
   // Add your routes here
+  this.route('activity', {
+    path: 'activity'
+  });
   this.route('search-results', {
     path: 'search/:term'
   });
