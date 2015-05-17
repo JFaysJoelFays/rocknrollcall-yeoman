b6f500dc1b316744dd25a43fcd0026ae098b3061
JFays <joel.fays@jfays.net>
2015-05-17 12:23:58 +0200

    Update SearchResultsController to include transitionToRoute.

--- app/scripts/controllers/search_results_controller.js
+++ app/scripts/controllers/search_results_controller.js
@@ -2,6 +2,18 @@ RocknrollcallYeoman.SearchResultsController = Em.ObjectController.extend({
 	artistsIsChecked: true,
 	songsIsChecked: true,
 	actions: {
+		viewedArtist: function(model) {
+			var date = Date.now();
+			var activity = this.store.createRecord('activity', {
+				display_id: model.enid,
+				type: model.type,
+				display_name: model.artist_name,
+				hotttnesss: model.hotttnesss,
+				timestamp: date
+			});
+			activity.save();
+			this.transitionToRoute('artist', model.enid);
+		},
 		viewedSong: function(model) {
 			var date = Date.now();
 			var activity = this.store.createRecord('activity', {
@@ -12,6 +24,7 @@ RocknrollcallYeoman.SearchResultsController = Em.ObjectController.extend({
 				timestamp: date
 			});
 			activity.save();
+			this.transitionToRoute('song', model.enid);
 		}
 	}
 });
