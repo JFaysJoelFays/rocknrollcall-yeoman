a85d05c6996bdbbe75c7110d18f415d81c5402fd
JFays <joel.fays@jfays.net>
2015-05-17 17:20:12 +0200

    Add an Application Initializer to seed the Ember Data Store with 300 activities.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,4 @@
+/// <reference path="../../typings/ember/ember.d.ts"/>
 var RocknrollcallYeoman = window.RocknrollcallYeoman = Ember.Application.create({
 	LOG_TRANSITIONS: true,
 	LOG_ACTIVE_GENERATION: true
@@ -28,6 +29,57 @@ require('scripts/routes/*');
 require('scripts/components/*');
 require('scripts/views/*');
 require('scripts/router');
+
+Ember.Application.initializer({
+	name: "DBSeeds",
+	initialize: function(container, application) {
+		
+		localStorage.clear();
+		
+		var store = container.lookup('store:main');
+		console.log('store: ', store);
+		
+		for (var i = 0; i < 300; i++) {
+			
+			var id = 'ARNH6LU1187FB430FA';
+			var random_id = id.split('').sort(function() {
+				return 0.5 - Math.random()
+			}).join('');
+			
+			var types = [
+				'song',
+				'artist'
+			];
+			
+			var random_type = Math.floor(Math.random() * types.length);
+			
+			var name = 'Willie Nelson';
+			var random_name = name.split('').sort(function() {
+				return 0.5 - Math.random()
+			}).join('');
+			
+			var random_hotness = Math.floor(Math.random() * 100) + 1;
+			var random_timestamp = new Date(new Date(2013, 9, 30).getTime() +
+					Math.random() * (new Date().getTime() - new Date(2013, 9, 30).getTime()));
+			
+			activity = store.createRecord('activity', {
+				display_id: random_id,
+				type: types[random_type],
+				display_name: random_name,
+				hotttnesss: random_hotness,
+				timestamp: random_timestamp
+			});
+			
+			activity.save();
+		}
+		
+		console.log(store.find('activity')
+			.then(function(stuff) {
+				console.log('Total Activity Records: ' + stuff.toArray().length)
+			})
+		);
+	}
+});
 
 Ember.Handlebars.helper('hotttnesss-badge', function(value, options) {
 	var h = parseFloat(value);
